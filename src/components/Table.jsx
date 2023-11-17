deaa083aba584d355b3475c659a51e4c5126e900
E J <johnsonelijah440@gmail.com>
2023-11-17 12:50:48 -0600

    <GA6>

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,12 +3,15 @@ const Table = ({ sat }) => {
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
-     <tr>
-       <td>Row Data TBD</td>
+      {sat.map((id, data) => <tr key={id.index}> <td>{data.name}</td> <td>{data.type}</td> <td>{data.launchDate}</td> <td>{data.operational}</td> </tr>)}
+     <tr key={sat.id}>
      </tr>
      </tbody>
    </table>
